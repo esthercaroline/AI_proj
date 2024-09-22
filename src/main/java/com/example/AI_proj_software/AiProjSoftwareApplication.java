0218172c1dd0aa5e3b0a4be044dac4c8b1d54337
Esther <estherccr@al.insper.edu.br>
2024-09-22 19:33:32 -0300

    build do projeto

---
+++ src/main/java/com/example/AI_proj_software/AiProjSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.example.AI_proj_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiProjSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiProjSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AI_proj_software/AiProjSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AI_proj_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiProjSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
